e0fd886cd83fd53eb227296c0928d1e4c1561210
Sukbin Lee <lunarlsb@gmail.com>
2023-01-18 00:58:32 +0900

    [Add] Create repository

---
+++ JavaScript/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+http.createServer((request, response) =>
+{
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello World');
+}
+).listen(5500);
+
+console.log('Server running at http://127.0.0.1:5500/');
